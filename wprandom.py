cd893f6759606a5c91ed869b0d514707104fe6ab
Duocor3 <tsegazeab12@gmail.com>
2020-09-27 15:33:31 -0400

    added one line of code the changes the wallpaper

---
+++ wprandom.py
@@ -0,0 +1,5 @@
+import ctypes
+
+#i have no idea what this does :p
+#C:\Users\admin\Pictures\Backgrounds\black hole.jpg
+ctypes.windll.user32.SystemParametersInfoW(20, 0, r"C:\Users\admin\Pictures\Backgrounds\black hole.jpg" , 0)
